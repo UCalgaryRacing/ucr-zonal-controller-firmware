c15c2eac230181cfec3bc5ba92fd57b3a411b08b
f-dunnwolbaum <claydunnw@gmail.com>
2026-06-25 13:57:45 -0600

    Added drivers and service for instrumentation module V2.1. Need testing

--- Core/controllers/rear_controller/config/instrumentation/ins_config_pinout.h
+++ Core/controllers/rear_controller/config/instrumentation/ins_config_pinout.h
@@ -8,4 +8,9 @@ extern TIM_HandleTypeDef htim2;
 #define INS_FRONT_WHEEL_TIMER_HANDLE           (&htim2)
 #define INS_FRONT_WHEEL_TIMER_CHANNEL          TIM_CHANNEL_1
 
+extern SPI_HandleTypeDef hspi1;
+
+#define INS_SPI_HANDLE (&hspi1)
+// mcu v1.0 does not support chip select pins for the instrumentation module
+
 #endif
